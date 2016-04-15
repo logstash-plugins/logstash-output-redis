50cfef524243a1c8e61428ddf38c4606888e051a
Tal Levy <jubboy333@gmail.com>
2016-04-15 09:40:23 -0700

    split out specs to unit and integration

--- spec/integration/outputs/redis_spec.rb
+++ spec/integration/outputs/redis_spec.rb
@@ -6,7 +6,7 @@ require "flores/random"
 
 describe LogStash::Outputs::Redis do
 
-  context "integration tests", :redis => true do
+  context "integration tests", :integration => true do
     shared_examples_for "writing to redis list" do |extra_config|
       let(:key) { 10.times.collect { rand(10).to_s }.join("") }
       let(:event_count) { Flores::Random.integer(0..10000) }
@@ -70,37 +70,6 @@ describe LogStash::Outputs::Redis do
           expect(redis_config).to include("batch")
           expect(redis_config["batch"]).to be_truthy
         end
-      end
-    end
-  end
-
-  context "Redis#receive in batch mode" do
-    # this is a regression test harness to verify fix for https://github.com/logstash-plugins/logstash-output-redis/issues/26
-    # TODO: refactor specs above and probably rely on a Redis mock to correctly test the code expected behaviour, the actual
-    # tests agains Redis should be moved into integration tests.
-    let(:key) { "thekey" }
-    let(:payload) { "somepayload"}
-    let(:event) { LogStash::Event.new({"message" => "test"}) }
-    let(:config) {
-      {
-        "key" => key,
-        "data_type" => "list",
-        "batch" => true,
-        "batch_events" => 50,
-       }
-    }
-    let(:redis) { described_class.new(config) }
-
-    it "should call buffer_receive" do
-      redis.register
-      expect(redis).to receive(:buffer_receive).exactly(10000).times.and_call_original
-      expect(redis).to receive(:flush).exactly(200).times
-
-      # I was able to reproduce the LocalJumpError: unexpected next exception at around 50
-      # consicutive invocations. setting to 10000 should reproduce it for any environment
-      # I have no clue at this point why this problem does not happen at every invocation
-      1.upto(10000) do
-        expect{redis.receive(event)}.to_not raise_error
       end
     end
   end
